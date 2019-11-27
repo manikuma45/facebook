6a8c870fd25c35f63ed1cf96d5c1baaed1f91cde
manish kumar <manikuma0405@gmail.com>
2019-11-27 18:21:00 +0900

    バリデーションと関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
   validates :name,  presence: true, length: { maximum: 30 }
-  validates :email, presence: true, length: { maximum: 255 },
-  			 format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :email, presence: true, length: { maximum: 255 }
+  #,
+  #			 format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validation { email.downcase! }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+  has_many :messages, dependent: :destroy #ユーザーはたくさんのメッセージを持ちます,ユーザーが削除されたらそのユーザーのメッセージも削除される。
+  has_many :nices, dependent: :destroy
 end
