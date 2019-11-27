53dcfe5b19a2bc3a833426318a16df253c2effb0
manish kumar <manikuma0405@gmail.com>
2019-11-27 18:21:45 +0900

    ルートの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  get '/messages', to: 'messages#index'
+#  get '/messages', to: 'messages#index'
+#resources :nices, only:[:create, :destroy]
+resources :messages do
+		collection do
+			post :confirm
+		end
+	end
 	resources :sessions, only: [:new, :create, :destroy]
 	resources :users
-	root to: "users#new"
+	root to: "sessions#new"
+	resources :nices, only:[:create, :destroy]
 end
